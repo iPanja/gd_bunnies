74f3fa8302151c572e804d15f5b9801faae7ce9a
Fletcher Henneman <fhenneman@pop-os.localdomain>
2023-09-06 19:02:12 -0400

    audio work

--- Scripts/UI/OptionsMenu.gd
+++ Scripts/UI/OptionsMenu.gd
@@ -3,8 +3,8 @@ class_name OptionsMenu
 
 signal back_pressed
 
-@onready var sfx_slider = %SFXSlider
-@onready var music_slider = $%MusicSlider
+@onready var sfx_toggle = %SFXToggle
+@onready var music_toggle = %MusicToggle
 @onready var back_button = $%BackButton
 
 var previous_ui: Node
@@ -12,28 +12,16 @@ var previous_ui: Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	back_button.pressed.connect(on_back_pressed)
-	sfx_slider.value_changed.connect(on_audio_slider_changed.bind("SFX"))
-	music_slider.value_changed.connect(on_audio_slider_changed.bind("Music"))
-	update_display()
+	sfx_toggle.toggled.connect(on_option_toggle.bind("SFX"))
+	music_toggle.toggled.connect(on_option_toggle.bind("Music"))
 
-func get_bus_volume_percent(bus_name: String):
-	var bus_index = AudioServer.get_bus_index(bus_name)
-	var volume_db = AudioServer.get_bus_volume_db(bus_index)
-	return db_to_linear(volume_db)
+#func on_audio_slider_changed(value: float, bus_name: String):
+#	set_bus_volume_percent(bus_name, value)
 
-func set_bus_volume_percent(bus_name: String, percent: float):
-	var bus_index = AudioServer.get_bus_index(bus_name)
-	var volume_db = linear_to_db(percent)
-	AudioServer.set_bus_volume_db(bus_index, volume_db)
-	GameSettings.write_back_volume_setting(bus_name, percent)
-
-func on_audio_slider_changed(value: float, bus_name: String):
-	set_bus_volume_percent(bus_name, value)
-	
-func update_display():
-	#sfx_slider.value = get_bus_volume_percent("SFX")
-	#music_slider.value = get_bus_volume_percent("Music")
-	pass
+func on_option_toggle(value: bool, bus_name: String):
+	print(bus_name)
+	print(value)
+	set_bus_volume_toggle(bus_name, value)
 
 func on_back_pressed():
 	back_pressed.emit()
