e8c4c7355a6a626fefb36453edea2c4dce1a2ee5
iPanja <fhenneman@comcast.net>
2023-11-16 00:32:03 -0500

    pause menu fix

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -8,14 +8,11 @@ var main_menu_scene: PackedScene = preload("res://Levels/Main.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("ready")
 	get_tree().paused = true
-	print("connected")
 	continue_button.connect("pressed", on_continue_pressed)
 	main_menu_button.connect("pressed", on_main_menu_pressed)
 
 func on_continue_pressed():
-	print("resume")
 	get_tree().paused = false
 	queue_free()
 
