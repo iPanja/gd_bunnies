e0081bcb0c938c9e80ed0dfd9c82567d05bbdc24
iPanja <fhenneman@comcast.net>
2023-11-16 02:33:42 -0500

    level replayability singleton fix - weakrefs

--- Scripts/Singletons/PMManager.gd
+++ Scripts/Singletons/PMManager.gd
@@ -1,19 +1,22 @@
 extends Node
 class_name PMManager
 
-var pms: Array[PieceManager]
+var pms: Array[WeakRef] # WeakRef[PieceManager]
 var biome: Biome = preload("res://Resources/Biomes/Sand.tres")
 
 func get_collision_pm(global_mouse_pos: Vector2) -> PieceManager:
-	for pm in pms:
-		if is_instance_valid(pm):
+	for wr_pm in pms:
+		var pm: PieceManager = wr_pm.get_ref()
+		if pm != null:
 			if pm.is_inside_grid_container(global_mouse_pos):
 				# Collision with this pm
 				return pm
-		else:
-			pms.erase(pm)
 	return null
 
 func register(pm: PieceManager) -> int:
-	pms.append(pm)
+	pms.append(weakref(pm))
+	cleanup_pms()
 	return pms.size()
+
+func cleanup_pms():
+	pms = pms.filter(func(wrpm): return wrpm.get_ref() != null)
